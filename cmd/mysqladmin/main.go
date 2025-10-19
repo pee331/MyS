c890946f7f0dbe5746165f62eeb3b6794d47217c
Peny <43066291+pee331@users.noreply.github.com>
2025-10-19 11:48:20 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/mysqladmin/main.go
@@ -0,0 +1,23 @@
+// cmd/mysqladmin/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "mysqladmin/internal/mysqladmin"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := mysqladmin.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
